af104aa6d4eee602b2987e6f5172f4c10ec23f1a
lhuerta13 <lucas.huerta@edu.univ-fcomte.fr>
2022-11-27 20:30:54 +0100

    Add todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div>React simple todo list</div>;
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+
+    // Ajoute la todo a la liste
+    // ... => ajoute la nouvelle todo aux éléments déjà existants
+    setList([...list, newTodo]);
+
+    // clear l'input
+    setInput("");
+  };
+  return (
+    <div>
+      <h1>Todo list</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></input>
+      <button onClick={() => addTodo(input)}>Add</button>
+    </div>
+  );
 }
 
 export default App;
